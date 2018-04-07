0b5228835e039b38d7803ce1b28cbc63777739d6
zlunchbox <zluettgen@gmail.com>
2018-04-07 14:11:03 -0400

    removed src; added all of xv6

--- pc.sh
+++
@@ -1,9 +0,0 @@
-#! /bin/bash
-
-cp ../xv6-public/defs.h ./src
-cp ../xv6-public/sysfile.c ./src
-cp ../xv6-public/fs.c ./src
-cp ../xv6-public/proc.h ./src
-cp ../xv6-public/proc.c ./src
-cp ../xv6-public/mmu.h ./src
-cp ../xv6-public/exec.c ./src
